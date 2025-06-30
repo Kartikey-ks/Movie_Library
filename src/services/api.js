386aeb4721fe578794999aa6b6dfea30c97b39d1
Kartikey Singh <kartikey.ks03@gmail.com>
2025-06-30 11:29:49 +0530

    changed api method

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,3 @@
-// const API_KEY = "ae4a97c89f9fbe41e9f55a935547dcf2";
-// const BASE_URL = "http://www.omdbapi.com/";
 
 // export const fetchMovies = async() => {
 //     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
@@ -7,16 +5,8 @@
 //     return data.results;
 // };
 
-// export const searchMovies = async(query) => {
-//     const response = await fetch(
-//         `${BASE_URL}/movie/popular?api_key=${API_KEY}&query=${encodeURIComponent(
-//             query
-//     )}`);
-//     const data = await response.json();
-//     return data.results;
-// };
 
-const API_KEY = "import.meta.env.VITE_OMDB_API_KEY";
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
 const BASE_URL = "https://www.omdbapi.com";
 
 // Function to search movies by title
